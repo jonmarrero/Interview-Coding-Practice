00ba039728be91c2ba943477621d229aeb1681d6
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-15 18:06:06 -0500

    day 4 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -124,3 +124,45 @@ var map = function(arr, fn) {
     // return the result 
     return res;
 };
+
+
+// Question 5 
+
+// Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+// The fn function takes one or two arguments:
+
+// arr[i] - number from the arr
+// i - index of arr[i]
+// filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+// Please solve it without the built-in Array.filter method.
+
+// solved using the array.filter method 
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    // declaritive programming 
+    return arr.filter(fn);
+};
+
+// solved without using array.filter method 
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    // imperative programming
+    for (const i in arr) {
+        // iterate through the array to see if the variable meets the statement 
+        if (fn(arr[i], Number(i))) {
+            res.push(arr[i])
+        }
+    }
+    return res;
+};
