2ba93b6e41d62f2b4a045a2641954104313b6a7a
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-20 15:33:37 -0500

    day 9 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -352,3 +352,43 @@ function memoize(fn) {
  * memoizedFn(2, 3) // 5
  * console.log(callCount) // 1 
  */
+
+
+//  Promises and Time 
+//  QUestion 10 
+
+// Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
+
+// The time limited function should follow these rules:
+
+// If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
+// If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        //  return a promise 
+        return new Promise((resolve, reject) => {
+            // pass in a method that calls reject 
+            // we are telling setTimeout to execute this code on this amount of time 
+            const id = setTimeout(() => reject("Time Limit Exceeded"), t);
+            fn(...args)
+            // use .then that accepts a function to get the result of fn(..args) 
+                .then((res) => resolve(res))
+                // there might be an error thrown so we want to catch it and call reject to catch the error 
+                .catch((err) => reject(err))
+        // run a cleartimeout function to stop the execution of the setTimeout function after the called period of time 
+        // .finally is a promise that handles the clearTimeout for us 
+                .finally(() => clearTimeout(id));
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
