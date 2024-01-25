fa3f0d70cdee160755ba20bc020b94258cc7d553
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-25 18:10:59 -0500

    day 14 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -595,3 +595,38 @@ var chunk = function(arr, size) {
     // then return the result 
     return res;
 };
+
+
+// Question 15 
+
+// Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+
+// A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array with that key.
+
+// The provided callback fn will accept an item in the array and return a string key.
+
+// The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
+
+// Please solve it without lodash's _.groupBy function.
+
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const res = {};
+    // group by keys 
+    // call fn on each element in input array by using this key word 
+    for (const obj of this) {
+        const key = fn(obj);
+        // if the result of the key already exist then it will stay as it is, if it is falsey it will return undefined and show the result as an empty array 
+        res[key] = res[key] || [];
+        // this key is already mapped to some array so we want to push onto that array with the obj 
+        res[key].push(obj)
+    }
+    return res;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
