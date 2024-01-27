98cb0fca3a1afaef7a0fd000ed419e388ceecec5
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-27 18:19:12 -0500

    day 16 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -664,3 +664,28 @@ var flat = function (arr, n) {
     // return value of helper function 
     return helper(arr, 0);
 };
+
+
+// Question 17 
+
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// You may assume the array is the output of JSON.parse.
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    // check if the lenght is zero 
+    if (this.length) {
+        // use this key word to refer to the array and length
+        return this[this.length - 1];
+    }
+    // other wise return -1 by default 
+    return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
