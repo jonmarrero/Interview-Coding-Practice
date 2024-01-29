7f4fb6ff3528d9eeed9b50a510a07bf018758266
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-01-28 19:06:03 -0500

    day 17 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -689,3 +689,33 @@ Array.prototype.last = function() {
  * const arr = [1, 2, 3];
  * arr.last(); // 3
  */
+
+
+// Question 18 
+
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe:(v) => {
+            if (v === val) return true;
+            else throw new Error("Not Equal"); 
+        },
+        notToBe:(v) => {
+            if (v !== val) return true;
+            else throw new Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
