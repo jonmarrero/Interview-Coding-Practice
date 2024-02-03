df34eb9bf1057268706b87e0155051ceed1d982d
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-02-03 13:01:40 -0500

    day 23 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -914,3 +914,24 @@ var promiseAll = async function(functions) {
  * const promise = promiseAll([() => new Promise(res => res(42))])
  * promise.then(console.log); // [42]
  */
+
+
+// Question 24
+
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(typeof(obj) == 'object') {
+        return Object.keys(obj).length == 0;
+    }else{
+        return obj.length == 0;
+    }
+};
