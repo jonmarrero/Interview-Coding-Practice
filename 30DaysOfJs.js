bcd12b174889be42617da7e15dfdbb9de12c16b0
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-02-06 09:45:04 -0500

    day 26 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -1000,3 +1000,68 @@ var join = function(arr1, arr2) {
     }
     return Object.values(result)
 };
+
+
+//  Question 27 
+
+// Design an EventEmitter class. This interface is similar (but with some differences) to the one found in Node.js or the Event Target interface of the DOM. The EventEmitter should allow for subscribing to events and emitting them.
+
+// Your EventEmitter class should have the following two methods:
+
+// subscribe - This method takes in two arguments: the name of an event as a string and a callback function. This callback function will later be called when the event is emitted.
+// An event should be able to have multiple listeners for the same event. When emitting an event with multiple callbacks, each should be called in the order in which they were subscribed. An array of results should be returned. You can assume no callbacks passed to subscribe are referentially identical.
+// The subscribe method should also return an object with an unsubscribe method that enables the user to unsubscribe. When it is called, the callback should be removed from the list of subscriptions and undefined should be returned.
+// emit - This method takes in two arguments: the name of an event as a string and an optional array of arguments that will be passed to the callback(s). If there are no callbacks subscribed to the given event, return an empty array. Otherwise, return an array of the results of all callback calls in the order they were subscribed.
+
+class EventEmitter {
+    // use eventMap to take an event which is a string and map that to a list of callbacks 
+    eventMap = {};
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        // check if the event has the property 
+        if(!this.eventMap.hasOwnProperty(eventName)) {
+            this.eventMap[eventName] = new Set();
+        }
+        // given an event we want to add the callback to the set corresponding to this event 
+        this.eventMap[eventName].add(callback);
+
+        // this is returning an object that we want to call unsubscribe
+        return {
+            unsubscribe: () => {
+                this.eventMap[eventName].delete(callback);
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        // were always going to take that array of values and returning it 
+        const res = [];
+        // we take all the callbacks from this event to populate it 
+        (this.eventMap[eventName] ?? [])
+        // iterate through every callback and execute that callback 
+            .forEach((callback) => res.push(callback(...args)))
+        return res;
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
