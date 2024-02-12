87af16fc8e95b6460629dd05d3c3cf020de18696
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2024-02-12 17:45:04 -0500

    day 29 of 30

--- 30DaysOfJs.js
+++ 30DaysOfJs.js
@@ -1171,3 +1171,35 @@ var compactObject = function(obj) {
 
     return compacted;
 };
+
+//  Question 30 
+
+// given a string containing just the characters and determine if the input string is valid. 
+
+// an input string is valid if:
+// open brackets must be closed by the same type of brackets 
+// open brackets must be closed in the correct order 
+
+// reminder - listen to breakdown tomorrow 
+
+var isVallid = function(s) {
+    const stack = [];
+    const parens = '() {} []';
+    let i = 0;
+
+    while(i < s.length) {
+        stack.push(s[i]);
+        i++;
+
+        let open = stack(stack.length - 2);
+        let closed = stack(stack.length - 1);
+
+        let potParens = open + closed;
+
+    if(parens.includes(potParens)) {
+        stack.pop();
+        stack.pop();
+    }
+    }
+    return stack.length === 0;
+};
